a82330bf5f5069cba53700be0d6d7615e543b0b5
Chris Halbert <christopher.halbert@gmail.com>
2018-04-17 08:06:44 -0400

    Fix configs per reqs.

--- src/nomadic.php
+++ src/nomadic.php
@@ -5,10 +5,4 @@ return [
     // These are the additional columns in the migrations table that migrations can append data to.
     'schema' => [
     ],
-
-    // Whether or not you want automated unit tests generated to verify the up/down functionality.
-    'tests' => false,
-
-    // Whether or not you want to add a post hook to your workflow - see readme for documentation.
-    'checkoutHook' => false
 ];
