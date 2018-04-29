1380ca107555fecc4f99994eefdf1b58d1c137d7
Chris Halbert <christopher.halbert@gmail.com>
2018-04-29 09:23:15 -0400

    [appendTraits] Use parent populator.

--- src/NomadicMigrationCreator.php
+++ src/NomadicMigrationCreator.php
@@ -14,14 +14,7 @@ class NomadicMigrationCreator extends MigrationCreator
 
     protected function populateStub($name, $stub, $table)
     {
-        $stub = str_replace('DummyClass', $this->getClassName($name), $stub);
-
-        // Here we will replace the table place-holders with the table specified by
-        // the developer, which is useful for quickly creating a tables creation
-        // or update migration from the console instead of typing it manually.
-        if (! is_null($table)) {
-            $stub = str_replace('DummyTable', $table, $stub);
-        }
+        $stub = parent::populateStub($name, $stub, $table);
 
         $traits = config('nomadic.traits');
 
