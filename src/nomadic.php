63f0f3ec1d70789b695582623c2f6665375b2a5a
Chris Halbert <christopher.halbert@gmail.com>
2018-04-29 09:20:56 -0400

    [appendTraits] Add configuration for traits.

--- src/nomadic.php
+++ src/nomadic.php
@@ -5,4 +5,9 @@ return [
     // These are the additional columns in the migrations table that migrations can append data to.
     'schema' => [
     ],
+
+    // These are traits that you can have appended to your migrations automatically
+    'traits' => [
+
+    ]
 ];
