98e4e44e4cf06aa8b92dfb656c72eeb3b95ae49a
Chris Halbert <christopher.halbert@gmail.com>
2018-04-29 09:21:43 -0400

    [appendTraits] Extend current stub populator.

--- src/NomadicMigrationCreator.php
+++ src/NomadicMigrationCreator.php
@@ -10,6 +10,43 @@ use Illuminate\Database\Migrations\MigrationCreator;
  */
 class NomadicMigrationCreator extends MigrationCreator
 {
+    const USE_STUB = "use %s;";
+
+    protected function populateStub($name, $stub, $table)
+    {
+        $stub = str_replace('DummyClass', $this->getClassName($name), $stub);
+
+        // Here we will replace the table place-holders with the table specified by
+        // the developer, which is useful for quickly creating a tables creation
+        // or update migration from the console instead of typing it manually.
+        if (! is_null($table)) {
+            $stub = str_replace('DummyTable', $table, $stub);
+        }
+
+        $traits = config('nomadic.traits');
+
+        if (!empty($traits) && is_array($traits)) {
+            $stub = $this->appendTraits($traits, $stub);
+        }
+
+        return $stub;
+    }
+
+    protected function appendTraits($stub, $traits)
+    {
+        $newlyIncluded = "";
+        $uses = array();
+        foreach ($traits as $trait) {
+            $trait .= sprintf(self::USE_STUB, $trait) . PHP_EOL;
+            $uses[] = array_pop(explode('\\', $trait));
+        }
+
+        $currentlyIncluded = sprintf(self::USE_STUB, NomadicMigration::class);
+        $stub = str_replace($currentlyIncluded, $currentlyIncluded . PHP_EOL . $newlyIncluded, $stub);
+        $stub = str_replace('// DummyTraits', sprintf(self::USE_STUB, implode(", ", $uses)), $stub);
+        return $stub;
+    }
+
     /**
      * Get the path to the stubs.
      * @return string
