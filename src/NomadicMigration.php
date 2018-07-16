7c82086ca2f11f9153ff52fa54794e75308ff99f
Chris Halbert <christopher.halbert@gmail.com>
2018-07-15 22:11:04 -0400

    [fullMigrationHookSupport] Added full hook support within the migration.

--- src/NomadicMigration.php
+++ src/NomadicMigration.php
@@ -2,6 +2,7 @@
 
 namespace ChrisHalbert\LaravelNomadic;
 
+use ChrisHalbert\LaravelNomadic\Hooks\NomadicMigrationHookInterface;
 use Illuminate\Database\Migrations\Migration;
 
 /**
@@ -10,6 +11,18 @@ use Illuminate\Database\Migrations\Migration;
  */
 abstract class NomadicMigration extends Migration
 {
+    const CONSTRUCT = 'construct';
+
+    const PRE_MIGRATE = 'preMigrate';
+
+    const POST_MIGRATE = 'postMigrate';
+
+    const PRE_ROLLBACK = 'preRollback';
+
+    const POST_ROLLBACK = 'postRollback';
+
+    const DESTRUCT = 'destruct';
+
     /**
      * Additional properties or column names in your migration table.
      * @var array
@@ -29,14 +42,48 @@ abstract class NomadicMigration extends Migration
     protected $fileName;
 
     /**
+     *
+     */
+    protected $migrationHooks = [
+        self::CONSTRUCT => [],
+        self::PRE_MIGRATE => [],
+        self::POST_MIGRATE => [],
+        self::PRE_ROLLBACK => [],
+        self::POST_ROLLBACK => [],
+        self::DESTRUCT => []
+    ];
+
+    /**
      * NomadicMigration constructor.
      * @param NomadicRepositoryInterface $repository The repository.
      */
-    public function __construct(NomadicRepositoryInterface $repository)
+    final public function __construct(NomadicRepositoryInterface $repository)
     {
         $this->properties = array();
         $this->repository = $repository;
         $this->fileName = basename((new \ReflectionClass($this))->getFileName(), '.php');
+        $configHooks = config('nomadic.hooks');
+        foreach ($this->migrationHooks as $hook => &$values) {
+            if (isset($configHooks[$hook]) && !is_array($configHooks[$hook])) {
+                throw new \Exception("Configs for nomadic hook `{$hook}` must an array.");
+            }
+            $values = $configHooks[$hook];
+        }
+        $this->runHooks(self::CONSTRUCT);
+    }
+
+    final public function up()
+    {
+        $this->runHooks(self::PRE_MIGRATE);
+        static::migrate();
+        $this->runHooks(self::POST_MIGRATE);
+    }
+
+    final public function down()
+    {
+        $this->runHooks(self::PRE_ROLLBACK);
+        static::rollback();
+        $this->runHooks(self::POST_ROLLBACK);
     }
 
     /**
@@ -77,6 +124,56 @@ abstract class NomadicMigration extends Migration
         }
 
         return $this->properties;
+    }
+
+    public function addHook($name, NomadicMigrationHookInterface $hook)
+    {
+        $this->verifyValidHook($name);
+
+        if (isset($this->migrationHooks[$name])) {
+            throw new \Exception("Invalid migration hook `{$name}`.");
+        }
+
+        $this->migrationHooks[$name][] = $hook;
+    }
+
+    public function getHooks($name)
+    {
+        $this->verifyValidHook($name);
+
+        return $this->migrationHooks[$name];
+    }
+
+    public function clearHooks($name)
+    {
+        $this->verifyValidHook($name);
+        $this->migrationHooks[$name] = [];
+    }
+
+    public function __destruct()
+    {
+        $this->execute(self::DESTRUCT);
+    }
+
+    protected function verifyValidHook($name)
+    {
+        if (isset($this->migrationHooks[$name])) {
+            throw new \Exception("Invalid migration hook `{$name}`.");
+        }
+    }
+
+    protected function runHooks($name)
+    {
+        $this->verifyValidHook($name);
+
+        foreach ($this->migrationHooks[$name] as $hook) {
+            $this->runHook($hook);
+        }
+    }
+
+    protected function runHook(NomadicMigrationHookInterface $hook)
+    {
+        $hook->execute();
     }
 
     /**
