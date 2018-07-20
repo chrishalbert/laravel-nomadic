010b8ba90465707d2d21c791dc76268a993138d1
Chris Halbert <christopher.halbert@gmail.com>
2018-07-20 07:26:22 -0400

    [fullMigrationHookSupport] Add tests to verify hooks.

--- tests/NomadicMigrationTest.php
+++ tests/NomadicMigrationTest.php
@@ -2,6 +2,8 @@
 
 namespace ChrisHalbert\LaravelNomadic;
 
+use ChrisHalbert\LaravelNomadic\Hooks\NomadicMigrationHookInterface;
+
 class NomadicMigrationTest extends \PHPUnit_Framework_TestCase
 {
     protected $migration;
@@ -15,20 +17,94 @@ class NomadicMigrationTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->repo->expects($this->once())
-            ->method('getProperties')
-            ->withAnyParameters()
-            ->willReturn($this->mockDbProperties());
-
         $this->migration = $this->getMockForAbstractClass(
             NomadicMigration::class,
-            [$this->repo],
-            ''
+            [$this->repo]
         );
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Configs for nomadic hook `construct` must be an array.
+     */
+    public function testInvalidConfigHookAssignment()
+    {
+        \ConfigMock::set('nomadic.hooks', ['construct' => 5]);
+        $this->getMockForAbstractClass(
+            NomadicMigration::class,
+            [$this->repo],
+            '',
+            true
+        );
+    }
+
+    public function testMigrationHooks()
+    {
+        $construct = [$this->getMockMigrationHook(NomadicMigration::CONSTRUCT)];
+        $preMigrate = [$this->getMockMigrationHook(NomadicMigration::PRE_MIGRATE)];
+        $postMigrate = [$this->getMockMigrationHook(NomadicMigration::POST_MIGRATE)];
+        $preRollback = [$this->getMockMigrationHook(NomadicMigration::PRE_ROLLBACK)];
+        $postRollback = [$this->getMockMigrationHook(NomadicMigration::POST_ROLLBACK)];
+        $destruct = [$this->getMockMigrationHook(NomadicMigration::DESTRUCT)];
+
+        \ConfigMock::set('nomadic.hooks', compact(
+            'construct',
+            'preMigrate',
+            'postMigrate',
+            'preRollback',
+            'postRollback',
+            'destruct'
+        ));
+
+        $hookedUpMigration = $this->getMockForAbstractClass(
+            NomadicMigration::class,
+            [$this->repo]
+        );
+
+        $this->expectOutputString('constructpreMigratepostMigratepreRollbackpostRollbackdestruct');
+        $hookedUpMigration->up();
+        $hookedUpMigration->down();
+        $hookedUpMigration->__destruct();
+    }
+
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Invalid migration hook `fubar`.
+     */
+    public function testVerifyValidHooksThrowsException()
+    {
+        $this->migration->clearHooks('fubar');
+    }
+
+    public function testMigrateRollbackDoNothing()
+    {
+        $this->assertNull($this->migration->migrate());
+        $this->assertNull($this->migration->rollback());
+    }
+
+    public function testAddHooksGetHooksClearHooks()
+    {
+        $this->migration->addHook(NomadicMigration::PRE_MIGRATE, $this->getMockMigrationHook(NomadicMigration::PRE_MIGRATE));
+        $this->migration->addHook(NomadicMigration::POST_MIGRATE, $this->getMockMigrationHook(NomadicMigration::POST_MIGRATE));
+        $this->assertNotEmpty($this->migration->getHooks(NomadicMigration::PRE_MIGRATE));
+        $this->assertNotEmpty($this->migration->getHooks(NomadicMigration::POST_MIGRATE));
+
+        $this->expectOutputString('preMigratepostMigrate');
+        // Verify the migration can run when added manually
+        $this->migration->up();
+
+
+        $this->migration->clearHooks(NomadicMigration::PRE_MIGRATE);
+        $this->migration->clearHooks(NomadicMigration::POST_MIGRATE);
+        $this->assertEmpty($this->migration->getHooks(NomadicMigration::PRE_MIGRATE));
+        $this->assertEmpty($this->migration->getHooks(NomadicMigration::POST_MIGRATE));
+        // Should not do anything else
+        $this->migration->up();
     }
 
     public function testSetAndGetProperty()
     {
+        $this->expectGetPropertiesCalled();
         $this->migration->setProperty('author', 'Chris');
         $this->assertEquals('Chris', $this->migration->getProperty('author', false));
         $this->assertEquals('DB Chris', $this->migration->getProperty('author'));
@@ -36,6 +112,7 @@ class NomadicMigrationTest extends \PHPUnit_Framework_TestCase
 
     public function testGetProperties()
     {
+        $this->expectGetPropertiesCalled();
         $this->assertEquals([], $this->migration->getProperties());
         $this->assertEquals($this->mockDbProperties(), $this->migration->getProperties(true));
     }
@@ -44,6 +121,15 @@ class NomadicMigrationTest extends \PHPUnit_Framework_TestCase
     {
         unset($this->repo);
         unset($this->migration);
+        \ConfigMock::reset();
+    }
+
+    protected function expectGetPropertiesCalled()
+    {
+        $this->repo->expects($this->once())
+            ->method('getProperties')
+            ->withAnyParameters()
+            ->willReturn($this->mockDbProperties());
     }
 
     protected function mockDbProperties()
@@ -53,5 +139,20 @@ class NomadicMigrationTest extends \PHPUnit_Framework_TestCase
             'batch' => 1,
             'author' => 'DB Chris'
         ];
+    }
+
+    protected function getMockMigrationHook($type)
+    {
+        $hook = $this->getMockBuilder(NomadicMigrationHookInterface::class)
+            ->setMockClassName($type)
+            ->setMethods(['execute'])
+            ->getMock();
+
+        $hook->expects($this->once())
+            ->method('execute')
+            ->willReturnCallback(function() use ($type) {
+                echo $type;
+            });
+        return $hook;
     }
 }
