0f9c62f80b829d711f55f00bf9e25bc08066d6e7
Chris Halbert <christopher.halbert@gmail.com>
2018-08-22 22:42:48 -0400

    [GH-18-TimestampableTrait] Fix test; after required, it returns bool true.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -3,7 +3,7 @@
 class ConfigTest extends PHPUnit_Framework_TestCase
 {
     public function testConfigHasKeys() {
-        $configs = require_once dirname(__FILE__) . '/../src/nomadic.php';
+        $configs = require dirname(__FILE__) . '/../src/nomadic.php';
         $this->assertArrayHasKey('schema', $configs);
         $this->assertArrayHasKey('traits', $configs);
     }
